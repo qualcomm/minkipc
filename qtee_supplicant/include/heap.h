0aa35d721c9c015a52400c9dd51d7748e851b43f
Harshal Dev <hdev@qti.qualcomm.com>
2025-05-14 17:51:18 +0530

    Add support for time listener and QTEE supplicant
    Added support for time listener library along with QTEE supplicant daemon
    which starts the time listener and waits for time requests from QTEE.
    
    Tests: Tested time listener request success via tzecotestapp TA on sm8650
    target.
    
    Tested-by: Harshal Dev <hdev@qti.qualcomm.com>
    Signed-off-by: Harshal Dev <hdev@qti.qualcomm.com>

---
+++ qtee_supplicant/include/heap.h
@@ -0,0 +1,26 @@
+// Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+// SPDX-License-Identifier: BSD-3-Clause-Clear
+
+#ifndef __HEAP_H
+#define __HEAP_H
+
+/**
+ * heap_port.h supplies definitions compatible with the following:
+ *
+ *   void *heap_zalloc(size_t size);
+ *   void *heap_calloc(size_t num, size_t size);
+ *   void  heap_free(void *ptr);
+ *   void *heap_memdup(const void *ptr, size_t size);
+ */
+#include "heap_port.h"
+
+#define HEAP_ZALLOC_TYPE(ty)      ((ty *) heap_zalloc(sizeof(ty)))
+
+#define HEAP_ZALLOC_ARRAY(ty, k)  ((ty *) heap_calloc((k), sizeof(ty)))
+
+#define HEAP_FREE_PTR(var)        ((void) (heap_free(var), (var) = 0))
+
+// Older convention:
+#define HEAP_ZALLOC_REC(ty)       HEAP_ZALLOC_TYPE(ty)
+
+#endif // __HEAP_H
