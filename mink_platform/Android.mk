bfc25e6311a8d6f5470a4b30c96e6e99f0e8d429
Jiaxing Li <jiaxing.li@oss.qualcomm.com>
2026-05-21 14:29:08 +0800

    mink: Temporarily disable Android compilation on LA
    Temporarily remove Android.bp and Android.mk build rules to disable
    minkipc compilation on LA for project integration. These build entries
    can be restored once the integration dependency is ready.
    
    Signed-off-by: Jiaxing Li <jiaxing.li@oss.qualcomm.com>

--- mink_platform/Android.mk
+++
@@ -1,6 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
-
-MINK_PLATFORM_PATH := $(LOCAL_PATH)
-include $(MINK_PLATFORM_PATH)/HLOS/minkdaemon/Android.mk
-include $(MINK_PLATFORM_PATH)/mink_test/Android.mk
--- mink_platform/mink_test/Android.mk
+++
@@ -1,3 +0,0 @@
-ifeq ($(TARGET_ENABLE_MINK_COMPONENT), true)
-include $(call all-subdir-makefiles)
-endif
